b1a27af943485a095c83c905874cc49c99c55028
Ana Lizbeth Martínez <82017492+almfic43@users.noreply.github.com>
2021-04-14 00:00:21 -0500

    Delete mi-footer.js

--- cmp/mi-footer.js
+++
@@ -1,7 +0,0 @@
-class MiFooter extends HTMLElement {
-  connectedCallback(){
-    this.innerHTML = /* html */
-  `Copyright &copy; 2020 Ana Lizbeth Martínez Francisco.`;
-  }
-}
-customElements.define("mi-footer", MiFooter);
